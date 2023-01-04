9a6e14f4ca8231d712c4bbbc08a4333b0b9822c8
권나영 <dek3127@gmail.com>
2023-01-04 18:56:21 +0900

    한글 UTF 설정 추가 및 MessageSource Test

---
+++ message-start/src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -0,0 +1,22 @@
+package hello.itemservice.message;
+
+import org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class MessageSourceTest {
+    @Autowired
+    MessageSource ms;
+
+    @Test
+    void helloMessage() {
+        // locale에 null 입력 시 basename(messages)따라 감.
+        String result = ms.getMessage("hello", null, null);
+        assertThat(result).isEqualTo("안녕");
+    }
+}
